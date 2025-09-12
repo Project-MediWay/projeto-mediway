a2271f9a41c0f64d6635a2a2a7f2d6fa265cbe68
ViniiGama <vinicius.gama@sptech.school>
2025-09-12 14:01:00 -0300

    Upgrade da calculadora

--- Banco de dados - Grupo/ATIVIDADE_GRUPO_10_SCRIPT_PROJETO.sql
+++ Banco de dados - Grupo/ATIVIDADE_GRUPO_10_SCRIPT_PROJETO.sql
@@ -9,10 +9,10 @@ Breno Abilio - 01252018
 Marcos Lopis Pereira - 01252034
 */
 
- -- usuario, empresa, medicamento, sensor, veiculo
 CREATE DATABASE Mediway;
 USE Mediway;
 
+-- CRIAÇÃO DA TABELA USUARIO
 CREATE TABLE usuario(
 idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(60) NOT NULL,
@@ -22,43 +22,51 @@ senha VARCHAR(20) NOT NULL,
 telefone VARCHAR(15)
 );
 
+-- INSERÇÃO DE DADOS NA TABELA USUARIO
 INSERT INTO usuario(nome, cpf, email, senha, telefone) VALUES
 	('João Silva','52598742411','joao@gmail.com','123456', '11985378011'),
     ('Ana Silva', '12345678901', 'ana.silva@email.com', 'senha123', '11987654321'),
     ('Carlos Souza', '98765432100', 'carlos.souza@email.com', 'minhaSenha', '21999887766');
-    
+
+-- CRIAÇÃO DA TABELA EMPRESA
 CREATE TABLE empresa(
 idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(50) NOT NULL,
 CNPJ VARCHAR(18) UNIQUE NOT NULL
 );
 
+-- INSERÇÃO DE DADOS NA TABELA EMPRESA
 INSERT INTO empresa (nome, CNPJ) VALUES 
 	('Farmácia Central LTDA', '12345678000195'),
     ('Translog Transportes SA', '98765432000188'),
     ('Saúde+ Distribuidora ME', '11223344000177');
 
+-- CRIAÇÃO DA TABELA MEDICAMENTO
 CREATE TABLE medicamento(
 idMedicamento INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(50) NOT NULL
 );
 
+-- INSERÇÃO DE DADOS NA TABELA MEDICAMENTO
 INSERT INTO medicamento (nome) VALUES 
     ('Vacina Pfizer'),
     ('Insulina Humana'),
     ('Eritropoetina');
 
+-- CRIAÇÃO DA TABELA SENSOR
 CREATE TABLE sensor(
 idtemperatura INT PRIMARY KEY AUTO_INCREMENT,
 temperatura_atual DECIMAL(4,2) NOT NULL,
 data_hora DATETIME NOT NULL
 );
 
+-- INSERÇÃO DE DADOS NA TABELA SENSOR
 INSERT INTO sensor (temperatura_atual, data_hora) VALUES
 	(5.0, '2025-08-26 10:00:00'),
 	(7.5, '2025-08-26 10:15:00'),
 	(1.2, '2025-08-26 11:00:00');
 
+-- CRIAÇÃO DA TABELA VEICULO
 CREATE TABLE veiculo(
 idVeiculo INT PRIMARY KEY AUTO_INCREMENT,
 modelo VARCHAR(30),
@@ -66,24 +74,22 @@ marca VARCHAR (30),
 tamanhoVeiculo VARCHAR (20),
 placa VARCHAR (7) UNIQUE,
 qtdMedicamento INT,
-CONSTRAINT chkTamanhoVeiculo CHECK (tamanhoVeiculo IN('Pequeno porte', 'Médio porte'))
+CONSTRAINT chkTamanhoVeiculo CHECK (tamanhoVeiculo IN('Pequeno porte', 'Médio porte', 'Grande porte'))
 );
 
-INSERT INTO veiculo (modelo, marca, tamanho_veiculo, placa) VALUES
+-- INSERÇÃO DE DADOS NA TABELA VEICULO
+INSERT INTO veiculo (modelo, marca, tamanhoVeiculo, placa) VALUES
 	('Accelo 1016', 'Mercedes-Benz', 'Médio porte', 'PQR3456'),
 	('VM 270', 'Volvo', 'Grande porte', 'STU7890'),
 	('Daily 35S14', 'Iveco', 'Pequeno porte', 'VWX1234');
 
+-- COMANDOS:
 SELECT * FROM usuario;
 SELECT * FROM empresa;
 SELECT * FROM medicamento;
-SELECT * FROM temperatura;
 SELECT * FROM veiculo;
 DESCRIBE usuario;
 DESCRIBE empresa;
 DESCRIBE medicamento;
-DESCRIBE temperatura;
 DESCRIBE veiculo;
 SHOW TABLES;
-
-
